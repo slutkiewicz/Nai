cdc4dbff8c06c7a5dd7f525fbd90ea9850c1ea04
Stanisław Lutkiewicz <stas.lutkiewicz@gmail.com>
2019-05-29 19:37:31 +0200

    all working

--- src/map.h
+++ src/map.h
@@ -6,30 +6,28 @@
 struct game_map_t {
     std::map< position_t/*CORDS*/ , node> nodes_Map;
         std::vector<std::string> t; // opis kafelkow
-  // # przeszkoda
-  // , ziemia
+  // # WALL
+  // , MARBLE
+  // . GRASS
+  // ; WATER
+  // : FIRE
 };
 
 enum event_enum { NONE, QUIT  };
 
 struct player_t {
- /*   player_t(){}
-    player_t( position_t playerPosition,std::shared_ptr<SDL_Texture> playerTexture){
-        position=playerPosition;
-        texture=playerTexture;
-    }
-*/
-    position_t position;
+    int id;
+   player_t(){
+       id=counter();
+   }
+
+    position_t position{};
     std::list<position_t> intention;
     std::shared_ptr<SDL_Texture> texture;
 };
 
 struct pointer_t {
-   /* pointer_t(){}
-    pointer_t( std::pair<position_t,game_enum > pointerPosition,std::shared_ptr<SDL_Texture> playerTexture){
-        position=pointerPosition;
-        texture=playerTexture;
-    }*/
+
     std::pair<position_t,game_enum > position;
 
     std::shared_ptr<SDL_Texture> texture;
@@ -62,14 +60,14 @@ process_input(std::shared_ptr<hardware_objects_t> hw,
 
 game_state_t
 calculate_next_game_state(const game_state_t &previous_state,
-                          std::pair<position_t,game_enum > intentions,
-                          double dt);
+                          std::pair<position_t,game_enum > intentions);
+
 void draw_player(SDL_Renderer *renderer, player_t &player);
 void draw_pointer(SDL_Renderer *renderer, pointer_t &pointer);
 
 void draw_world(SDL_Renderer *renderer, game_state_t &state);
 
-
+void setup_Bots(game_state_t* level);
 
 std::list<position_t> position_mapper(std::list<node*> nodes);
 
